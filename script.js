d11290f047e76306c9d32cac3cd7fb36e7d83311
annikagashi123 <annikagashi18@gmail.com>
2025-01-21 10:42:23 -0800

    repeatwordnumramaimah

---
+++ script.js
@@ -0,0 +1,47 @@
+// let vitiLidjes = prompt("shkruaj vitin e lindjes");
+
+// function kalkuloMoshen(vlera) {
+//     console.log(2025 - vlera);
+// }
+// kalkuloMoshen(vitiLidjes);
+
+
+
+// let vitiLidjes = prompt("shkruaj vitin e lindjes");
+
+// function kalkuloMoshen(vlera) {
+//    let rezultati = 2025 - vlera;
+//    return rezultati;
+// }
+//  let mosha = kalkuloMoshen(vitiLidjes);
+//  console.log("variable mosha" + mosha);
+
+
+
+//3  gabim te numrimeimdh detyra ke gjeje numrin ma t madhe
+
+// function numriMeiMadh(numri1 , numri2)
+// if (numri1 > numri2) {
+//     return numri1;
+// } else if (numri1 == numri2) {
+//     return "numrat jane te barabarte";
+// }else {
+//     return numri2;
+// }
+// console.log(numriMeiMadh(3,5));
+
+//4 rpepatword
+function repeatWord(fjala, numriiPerseritjeve) {
+    if (fjala == "scantech"){
+        console.log("nuk e printojme kete fjale");
+        return;
+    }
+    for (let i = 1; i <= numriiPerseritjeve; i++) {
+        console.log(fjala);
+    }
+ }
+
+ repeatWord("scantech", 15);
+ repeatWord("mirjeta292", 10);
+ repeatWord("a", 100);
+ repeatWord(112345, 4);
