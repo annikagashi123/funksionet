a2e162c34768db581fa812b5c25a7b710dea2819
annikagashi123 <annikagashi18@gmail.com>
2025-01-27 10:46:48 -0800

    dfae

--- script.js
+++ script.js
@@ -45,3 +45,104 @@
 //  repeatWord("mirjeta292", 10);
 //  repeatWord("a", 100);
 //  repeatWord(112345, 4);
+
+
+
+
+//5
+
+
+
+// function guesstheword(fjala) {
+//     let fjalaEzgjedhur = "scantech";
+//     if (fjalaEzgjedhur == fjala){
+//         alert("urime e gjete");
+//     }
+//     else {
+//         alert("nuk e keni gjetur");
+//     }
+// }
+// let fjalaeuserit = prompt("qellojs fjales");
+// guesstheword(fjalaeuserit);
+
+//referenv
+// let stringu1 = "Mirjeta";
+// console.log(stringu1.length);
+// for(let i = stringu1.length - 1;i = >= 0; i --) {
+//     console.log(stringu1[i]);
+// }
+
+
+//gabim infinet loop
+// function aKa(fjala){
+//     console.log(fjala.length)
+//     for(let i = fjala.length ; i >= 0; i--) {
+//         console.log(fjala[i]);
+//     }
+// }
+// aKa("mirjeta");
+
+
+//i qet ne console rren 
+// function aKa(fjala){
+//     console.log(fjala.length)
+//     for(let i = 0 ; i < fjala.length; i++) {
+//         console.log(fjala[i]);
+//     }
+// }
+// aKa("mirjeta");
+
+
+//i qet consone mrpapsht
+// function aKa(fjala){
+//     console.log(fjala.length)
+//     for(let i = fjala.length ; i >= 0 ; i--) {
+//         console.log(fjala[i]);
+//     }
+// }
+// aKa("mirjeta");
+
+//6
+
+// function aKa(fjala){
+//        for(let i = 0 ; i < fjala.length; i++) {
+//          if (fjala[i]=== "a" ) {
+//             return true
+//          }
+//         }
+//     }
+    
+    
+    
+//    console.log (aKa("mirjrgseyqwadreta") );
+
+
+//7
+
+function akAnumracift(varg) {
+    let rezultati = false;
+for (let i = 0; i < varg.length; i++){
+    if (varg[i] % 2 === 0 ){
+        rezultati = true;
+        return;
+    }
+    else if (varg[i] % 2 !== 0){
+        rezultati = false;
+    }
+}
+return rezultati
+}
+ console.log(akAnumracift([1,2,3,43,54,2,4,6,8,7]) ) ;
+
+ function akAnumracift(varg) {
+for (let i = 0; i < varg.length; i++){
+    if (varg[i] % 2 === 0 ){
+    
+    }
+    else if (varg[i] % 2 !== 0){
+       
+    }
+}
+return rezultati
+}
+ console.log(akAnumracift([1,2,3,43,54,2,4,6,8,7]) ) ;
